16e5f6eed1306d365a8d9a43a9f3a87b35061ece
Don Williamson <dwilliamson_coder@hotmail.com>
2013-10-22 11:40:25 +0100

    Allow Bind to take anonymous nested functions as input: * Identify function parameters and separate function lookup from scope.

--- Core/Code/Bind.js
+++ Core/Code/Bind.js
@@ -18,11 +18,19 @@
 //    var b = Bind(this, "ThisFunction");
 //    var c = Bind("GlobalFunction", BoundParam0, BoundParam1);
 //    var d = Bind(this, "ThisFunction", BoundParam0, BoundParam1);
+//    var e = Bind(GlobalFunction);
+//    var f = Bind(this, ThisFunction);
+//    var g = Bind(GlobalFunction, BoundParam0, BoundParam1);
+//    var h = Bind(this, ThisFunction, BoundParam0, BoundParam1);
 //
 //    a(0, 1, 2);
 //    b(0, 1, 2);
 //    c(2);
 //    d(2);
+//    e(0, 1, 2);
+//    f(0, 1, 2);
+//    g(2);
+//    h(2);
 //
 function Bind()
 {
@@ -30,22 +38,46 @@ function Bind()
 	if (arguments.length == 0)
 		return null;
 
-	// Locate the function and its scope within the argument list
-	var scope = window;
-	var func = arguments[0];
-	var start = 1;
-	if (typeof(arguments[0]) != "string")
+	// Figure out which of the 4 call types is being used to bind
+	// Locate scope, function and bound parameter start index
+
+	if (typeof(arguments[0]) == "string")
 	{
-		scope = arguments[0];
-		func = arguments[1];
-		start = 2;
+		var scope = window;
+		var func = window[arguments[0]];
+		var start = 1;
+	}
+
+	else if (typeof(arguments[0]) == "function")
+	{
+		var scope = window;
+		var func = arguments[0];
+		var start = 1;
+	}
+
+	else if (typeof(arguments[1]) == "string")
+	{
+		var scope = arguments[0];
+		var func = scope[arguments[1]];
+		var start = 2;
+	}
+
+	else if (typeof(arguments[1]) == "function")
+	{
+		var scope = arguments[0];
+		var func = arguments[1];
+		var start = 2;
+	}
+
+	else
+	{
+		// unknown
+		console.log("Bind() ERROR: Unknown bind parameter configuration");
+		return;
 	}
 
 	// Convert the arguments list to an array
 	var arg_array = Array.prototype.slice.call(arguments, start);
-
-	// Lookup the function and mark where argument concatenation begins
-	func = scope[func];
 	start = arg_array.length;
 
 	return function()
